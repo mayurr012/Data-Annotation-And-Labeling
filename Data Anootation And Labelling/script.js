38b284c5b6416994ed51ab2a0b43818c5cb1ee1e
Mayur Surwase <surwasemayur12@gmail.com>
2025-01-10 19:09:22 +0530

    Update script.js

--- Data Anootation And Labelling/script.js
+++ Data Anootation And Labelling/script.js
@@ -1,6 +1,6 @@
 const canvas = document.getElementById('imageCanvas');
 const ctx = canvas.getContext('2d')
-const imageLoader = document.getElementById('imageLoader');
+const imageLoader = document.getElementById('imageloader');
 let image = new Image();
 let annotations = [];
 
